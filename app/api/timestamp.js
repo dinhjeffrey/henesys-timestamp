64d4af98a2b0aae3adde833fa36741a1a35f5af1
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 17:37:40 +0000

    updated timestamp

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -4,33 +4,41 @@ var moment = require('moment')
 module.exports = function(app) {
     app.get('/:query', function(req, res){
         var date = req.params.query
-        var unix = null
-        var natural = null
         
-    // check for initial unix time
-    if (+date >= 0) {
-        unix = +date;
-        natural = unixToNat(unix)
-    }
+        var unix = moment("MM/D/YYYY").unix();
+        var natural = moment.unix(date).format("MM/DD/YYYY");
+        var dateObj = { "unix": unix, "natural": natural }
+        res.send(JSON.stringify(dateObj))
+        
+        
+
+    //     var unix = null
+    //     var natural = null
+        
+    // // check for initial unix time
+    // if (+date >= 0) {
+    //     unix = +date;
+    //     natural = unixToNat(unix)
+    // }
     
-    // check for initial natural time
-    if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
-        unix = natToUnix(date)
-        natural = unixToNat(unix)
-    }
+    // // check for initial natural time
+    // if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+    //     unix = natToUnix(date)
+    //     natural = unixToNat(unix)
+    // }
     
-    var dateObj = { "unix": unix, "natural": natural }
-    res.send(JSON.stringify(dateObj))
+    // var dateObj = { "unix": unix, "natural": natural }
+    // res.send(JSON.stringify(dateObj))
     
-    })
+    // })
     
-    function natToUnix(date) {
-        //Convert from natural date to unix timestamp
-        return moment(date, "MMMM D, YYYY").format("X")
-    }
+    // function natToUnix(date) {
+    //     //Convert from natural date to unix timestamp
+    //     return moment(date, "MMMM D, YYYY").format("X")
+    // }
     
-    function unixToNat(unix) {
-        // Convert unix timestamp to natural date
-        return moment.unix(unix).format("MMMM D, YYYY")
-    }
+    // function unixToNat(unix) {
+    //     // Convert unix timestamp to natural date
+    //     return moment.unix(unix).format("MMMM D, YYYY")
+    // }
 }
